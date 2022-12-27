f8d1072c8ae6f8186d22acf0599256007649d0d8
Dmitry <stepanov.stal@yandex.ru>
2022-12-27 11:20:29 +0300

    z56 and z58

---
+++ z56/Program.cs
@@ -0,0 +1,65 @@
+﻿Console.Clear();
+// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int GetSize(string text)
+{
+    Console.Write(text);
+    int n = int.Parse(Console.ReadLine());
+    return n;
+}
+
+int[,] GetArr(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i,j] = new Random().Next(0, 10);
+        }
+    }
+    return arr;
+}
+
+void PrintArr(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+void GetMinSumm(int[,] arr)
+{
+    int indexMin = 0;
+    int index = 0;
+    int res = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < arr.GetLength(1); j++)  sum = sum + arr[i,j];
+        if (i == 0) res = sum;
+        if (sum < res)
+        {
+            res = sum;
+            indexMin = index;
+        }
+        index++;
+    }    Console.WriteLine($"номер строки c наименьшей суммой ({res}) элементов: {indexMin} строка");
+}
+
+
+int[,] arr = new int[GetSize("кол-во строк: "), GetSize("кол-во столбцов: ")];
+PrintArr(GetArr(arr));
+GetMinSumm(arr);
---
+++ z58/Program.cs
@@ -0,0 +1,73 @@
+﻿Console.Clear();
+// Задача 58: Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+int EnterData(string text)
+{
+    Console.Write(text);
+    int n = int.Parse(Console.ReadLine());
+    return n;
+}
+
+int[,] GetMatrix(string text)
+{
+    Console.WriteLine(text);
+    int[,] matrix = new int[EnterData("enter row: "), EnterData("enter coloumn: ")];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix (int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] matrixA = GetMatrix("Введите размер матрицы A:");
+PrintMatrix(matrixA);
+Console.WriteLine();
+int[,] matrixB = GetMatrix("Введите размер матрицы B:");
+PrintMatrix(matrixB);
+
+int m = matrixA.GetLength(0); // строка 
+int k = matrixB.GetLength(1);
+int n = matrixB.GetLength(0);
+int[,] matrixC = new int[m, k];
+
+
+int[,] ResultMatrix (int[,] c, int[,] a, int[,] b, int m, int k, int n)
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < k; j++)
+        {
+            int sum = 0;
+            for (int kk = 0; kk < n; kk++)
+            {
+                sum = sum + a[i,kk] * b[kk,j];
+            }
+            c[i, j] = sum;
+        }
+    }
+    return c;
+}
+Console.WriteLine("Результирующая матрица будет:");
+
+PrintMatrix(ResultMatrix(matrixC, matrixA, matrixB, m, k, n));
