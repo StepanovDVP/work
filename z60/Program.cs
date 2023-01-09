cc3d2ab56fa024c1d21241344c230b9db4f64074
Dmitry <stepanov.stal@yandex.ru>
2023-01-09 15:28:06 +0300

    dz9

--- z60/Program.cs
+++ z60/Program.cs
@@ -1,39 +1,44 @@
 ﻿Console.Clear();
-
-
-int[,] GetMatrix(int [,] mat)
-{
-       int i = 0; int j = 0; int di = 0; int dj = 1;
-       for (int k = 0; k < 16; k++)
-       {
-            mat[i,j] = k + 1;
-            int x = (i + di) % 4;
-            int y = (j + dj) % 4;
-            if (mat[x,y] != 0)
-            {
-                int temp = di;
-                di = dj;
-                dj = -temp;
-            }
-            i = i + di;
-            j = j + dj;
-       }
-       return mat;
-}
-void PrintMatrix (int[,] arr)
-{
-    for (int i = 0; i < arr.GetLength(0); i++)
-    {
-        for (int j = 0; j < arr.GetLength(1); j++)
-        {
-            Console.Write($"{arr[i, j]} ");
-        }
-        Console.WriteLine();
-    }
+int NumberGet(string text)
+{   
+    Console.Write(text);
+    int n = int.Parse(Console.ReadLine());
+    return n;
 }
 
-int[,] matrix = new int[4,4];
+Console.WriteLine("*** Задача 64 ***");
+// Задача 64: Задайте значение N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+string NumbersRec(int N)
+{
+    if(N>=1) return $"{N} " + NumbersRec(N-1);
+    else return String.Empty;
+}
 
-PrintMatrix(matrix);
-// PrintMatrix(GetMatrix(matrix));
-// GetMatrix(matrix);
+Console.WriteLine(NumbersRec(NumberGet("Введите число N: ")));
+
+Console.WriteLine("*** Задача 66 ***");
+// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+int SumRec(int m, int n)
+{
+    if (m - 1 == n) return 0;
+    else return n + SumRec(m, n - 1);
+}
+
+Console.WriteLine(SumRec(NumberGet("Введите число m: "), NumberGet("Введите число n: ")));
+
+Console.WriteLine("*** Задача 68 ***");
+// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int Ackermann(int m, int n)
+{
+    if (m == 0) return n + 1;
+    if ((m > 0) && (n == 0)) return Ackermann(m - 1, 1);
+    else return Ackermann(m - 1, Ackermann(m, n - 1));
+}
+
+Console.WriteLine(Ackermann(NumberGet("Введите число m: "), NumberGet("Введите число n: ")));
